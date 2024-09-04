108ac0f596559ec0ad83987cdc392a59f070df06
Subeen2 <snb0768@naver.com>
2024-09-04 22:05:39 +0900

    feat: error, not-found 페이지

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,1 +1,14 @@
-export {};
+"use client";
+
+import React from "react";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <p>We encountered an error. Please try again later.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
