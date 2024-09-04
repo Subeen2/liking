e30d8722401abc6ba51f5f90f46341a124d6fe06
Subeen2 <snb0768@naver.com>
2024-09-04 21:23:17 +0900

    feat: input 태그 커스텀 훅 작성

--- src/hooks/useInputChange.ts
+++ src/hooks/useInputChange.ts
@@ -1,1 +1,28 @@
-export {};
+import { useState } from "react";
+
+export const useInputChange = <T extends Record<string, any>>(
+  defaultValues: T
+) => {
+  const [values, setValues] = useState<T>(defaultValues);
+
+  const handler = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const reset = () => {
+    setValues(defaultValues);
+  };
+
+  const setValueInit = (value: any) => {
+    setValues(value);
+  };
+
+  return { setValues, values, handler, setValueInit, reset };
+};
