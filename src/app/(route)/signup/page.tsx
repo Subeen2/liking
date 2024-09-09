a59ab5642ee76b16e0315419e2640e30c3e86512
Subeen2 <snb0768@naver.com>
2024-09-09 16:13:11 +0900

    feat: 회원가입 폼 수정

--- src/app/(route)/signup/page.tsx
+++ src/app/(route)/signup/page.tsx
@@ -1,61 +1,66 @@
-import React from "react";
+import InputField from "@/components/inputField/field/InputField";
+import { useInput } from "@/hooks/useInput";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 export default function SignUp() {
+  // use client + next + router 사용 시 navigation 임포트 해야 함.
+  const router = useRouter();
+
+  const [error, setError] = useState({
+    nicknameError: "",
+    emailError: "",
+    passwordError: "",
+  });
+
+  const { values: input, handler: onChangeInput } = useInput({
+    nickname: "",
+    email: "",
+    password: "",
+  });
+
+  const [passwordHide, setPasswordHide] = useState(true);
+
+  const { nickname, email, password } = input;
+
   return (
     <div className="flex flex-col text-center py-xl">
       <h1 className="text-3xl font-bold  mb-[70px]">회원가입</h1>
       <div className="flex flex-row w-full justify-center text-left">
         <form className="flex flex-col justify-center gap-6 w-full mb-6 max-w-lg">
           <div className="flex flex-col">
-            <label
-              className="ml-1 mb-2 font-bold text-[16px]"
-              htmlFor="nickname"
-            >
-              닉네임
-            </label>
-            <div className="relative">
-              <input
-                className="h-[48px] px-4 w-full rounded-lg border md:text-[16px] text-[14px] placeholder-gray-2 focus:outline-none focus:border-gray-3 transition border-gray-2"
-                type="text"
-                placeholder="라이킹에서 사용할 닉네임을 적어주세요"
-                id="nickname"
-                name="nickname"
-                value=""
-              />
-            </div>
+            <InputField
+              label="닉네임"
+              type={"text"}
+              value={nickname}
+              name="nickname"
+              placeholder="사용할 닉네임을 입력해주세요"
+              onChange={onChangeInput}
+              error={error.nicknameError}
+            />
           </div>
           <div className="flex flex-col">
-            <label className="ml-1 mb-2 font-bold text-[16px]" htmlFor="email">
-              이메일
-            </label>
-            <div className="relative">
-              <input
-                className="h-[48px] px-4 w-full rounded-lg border md:text-[16px] text-[14px] placeholder-gray-2 focus:outline-none focus:border-gray-3 transition border-gray-2"
-                type="text"
-                placeholder="라이킹에서 사용할 이메일을 적어주세요"
-                id="email"
-                name="email"
-                value=""
-              />
-            </div>
+            <InputField
+              label="이메일"
+              type="text"
+              value={email}
+              name="email"
+              placeholder="이메일 주소를 입력해주세요"
+              onChange={onChangeInput}
+              error={error.emailError}
+            />
           </div>
           <div className="flex flex-col">
-            <label
-              className="ml-1 mb-2 font-bold text-[16px]"
-              htmlFor="password"
-            >
-              비밀번호
-            </label>
-            <div className="relative">
-              <input
-                className="h-[48px] px-4 w-full rounded-lg border md:text-[16px] text-[14px] placeholder-gray-2 focus:outline-none focus:border-gray-3 transition border-gray-2"
-                type="text"
-                placeholder="라이킹에서 사용할 비밀번호를 적어주세요"
-                id="password"
-                name="password"
-                value=""
-              />
-            </div>
+            <InputField
+              label="비밀번호"
+              type={passwordHide ? "password" : "text"}
+              value={password}
+              name="password"
+              placeholder="비밀번호를 입력해주세요"
+              onChange={onChangeInput}
+              error={error.passwordError}
+              setPasswordHide={setPasswordHide}
+            />
           </div>
           <button
             type="submit"
