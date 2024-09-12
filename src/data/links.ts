ce07662492113e9796d8573c66d4cc14869fb1f7
Subeen2 <snb0768@naver.com>
2024-09-12 16:27:17 +0900

    setting: link (사이트맵) 상수

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,1 +1,8 @@
-export {};
+export const mypageMenu = [
+  { href: `/mypage`, label: "내 프로필" },
+  { href: `/mypage/linking-list`, label: "링킹 리스트" },
+  { href: `/mypage/interests`, label: "나의 관심사" },
+  { href: `/mypage/likegroup`, label: "좋아요" },
+];
+
+// TODO :: 라우트 주소 수정 필요 , 통일 필요
