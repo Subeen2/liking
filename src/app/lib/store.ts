40ebf13fb00e02ef409b71ce902c52706741c7b6
Subeen2 <snb0768@naver.com>
2024-09-12 17:43:21 +0900

    fix: try-catch 문 추가

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -2,22 +2,32 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
 const loadState = () => {
-  const serializedState = localStorage.getItem("reduxState");
-  if (serializedState === null) {
+  try {
+    const serializedState = localStorage.getItem("reduxState");
+    if (serializedState === null) {
+      return undefined;
+    }
+
+    const parsedState = JSON.parse(serializedState);
+
+    // 로컬 스토리지의 상태를 rootReducer의 구조에 맞게 변환
+    return {
+      userAuth: parsedState, // userAuth로 감싸기
+    };
+  } catch (e) {
+    console.log("Could not load state", e);
     return undefined;
   }
-
-  const parsedState = JSON.parse(serializedState);
-
-  // 로컬 스토리지의 상태를 rootReducer의 구조에 맞게 변환
-  return {
-    userAuth: parsedState, // userAuth로 감싸기
-  };
 };
 
 const saveState = (state: RootState) => {
-  const serializedState = JSON.stringify(state.userAuth); // userAuth만 저장
-  localStorage.setItem("reduxState", serializedState);
+  try {
+    const serializedState = JSON.stringify(state.userAuth); // userAuth만 저장
+    localStorage.setItem("reduxState", serializedState);
+  } catch (e) {
+    console.log("Could not save state", e);
+    return undefined;
+  }
 };
 
 const preloadedState = loadState();
