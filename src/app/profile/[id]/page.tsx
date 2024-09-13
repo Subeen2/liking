da13b2d5e518d7a4ddd557f5417896cde52938f9
Subeen2 <snb0768@naver.com>
2024-09-13 14:11:46 +0900

    feat: 유저 프로필 페이지

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { RootState } from "@/app/lib/store";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function UserProfilePage({
   params,
@@ -6,13 +9,30 @@ export default function UserProfilePage({
   params: { id: string };
 }) {
   const { id } = params;
+  const router = useRouter();
 
-  // 여기에 API 호출이나 데이터를 받아오는 로직을 추가할 수 있습니다.
-  // 예를 들어, 특정 ID에 해당하는 사용자 정보를 가져올 수 있습니다.
+  // Redux에서 user 정보를 가져옵니다.
+  const user = useSelector((state: RootState) => state.userAuth);
+  const userUID = user.user?.uid?.user_uid;
+
+  // 사용자 ID와 Redux에 저장된 ID가 일치하는지 확인
+  useEffect(() => {
+    if (!user || userUID !== id) {
+      // 사용자 정보가 없거나 ID가 일치하지 않으면 로그인 페이지로 리다이렉트
+      router.push("/login");
+    }
+  }, [user, id, router]);
+
+  if (!user || userUID !== id) {
+    return <p>Loading...</p>; // 데이터를 불러올 때까지 로딩 표시
+  }
+
   return (
     <div>
-      <h1>User Profile</h1>
-      <p>Profile ID: {id}</p>
+      <h1>기본정보</h1>
+      <p>닉네임: {user.nickname}</p>
+      <p>이메일: {user.email}</p>
+
       <a href={`/profile/${id}/edit`}>Edit Profile</a>
     </div>
   );
