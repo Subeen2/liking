23d8c8e2d5d00bb0fa4cf2682d6ff9413ac92f67
Subeen2 <snb0768@naver.com>
2024-09-15 19:36:23 +0900

    css: 수정

--- src/components/button/TopButton.tsx
+++ src/components/button/TopButton.tsx
@@ -8,16 +8,17 @@ function TopButton() {
       behavior: "smooth",
     });
   };
+
   return (
     <button
       onClick={topBtn}
-      className="inline-flex justify-center items-center fixed z-50 right-4 md:right-[10%] bottom-[104px] md:bottom-[112px] border-solid"
+      className="inline-flex justify-center items-center fixed z-50 right-4 md:right-[10%] bottom-[104px] md:bottom-[112px] rounded-3xl p-1 px-1.5 border-2 "
     >
       <Image
         src="/icons/arrow-up-solid.svg"
         alt="top 스크롤 버튼"
-        width={20}
-        height={20}
+        width={15}
+        height={10}
       />
     </button>
   );
