7f74ce12a7dacd3861f0a1e746b6debd639e065f
Subeen2 <snb0768@naver.com>
2024-10-18 14:04:44 +0900

    feat: optimistic update with query

--- src/app/lib/rootReducer.ts
+++ src/app/lib/rootReducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-// 이거 꼭 이런 형식으로 작성해야 함.!!!!
+// 이거 꼭 이런 형식으로 임포트해야 함.!!!!
 import userAuthReducer from "./features/userAuth/userAuthSlice";
 
 const rootReducer = combineReducers({
